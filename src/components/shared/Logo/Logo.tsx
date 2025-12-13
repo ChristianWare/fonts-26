c6237404fcde6dceb749e87bdd9d7fcdb99645eb
Chris Ware <c.p.ware@hotmail.com>
2025-12-13 13:25:01 -0700

    logo updated

--- src/components/shared/Logo/Logo.tsx
+++ src/components/shared/Logo/Logo.tsx
@@ -1,17 +1,11 @@
 import styles from "./Logo.module.css";
 import Link from "next/link";
-import Image from "next/image";
-import Img1 from "../../../../public/logos/logoYellow.png";
+
 
 const Logo = () => {
   return (
-    <Link href='/' className={styles.logo}>
-      <div className={styles.imgContainer}>
-        <Image src={Img1} alt='Logo' fill className={styles.img} />
-      </div>
-      <section className={styles.container}>
-        <span className={styles.text}>Fonts & Footers</span>
-      </section>
+    <Link href='/' className={styles.text}>
+      Fonts & Footers
     </Link>
   );
 };
