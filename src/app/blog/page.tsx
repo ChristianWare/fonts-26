a604595333fe0f6d9b0f1b5af6cabcd6d8e227b8
Chris Ware <c.p.ware@hotmail.com>
2025-12-13 17:28:34 -0700

    added sanity

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,13 @@
+import AllBlogsPosts from "@/components/BlogPage/AllBlogsPosts/AllBlogsPosts";
 import BlogHero from "@/components/BlogPage/BlogHero/BlogHero";
+import FinalCTAMain from "@/components/shared/FinalCTAMain/FinalCTAMain";
 
 export default function BlogPage() {
   return (
     <main>
       <BlogHero />
+      <AllBlogsPosts />
+      <FinalCTAMain />
     </main>
   );
 }
