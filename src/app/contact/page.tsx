6ecc132bdc20395fb794086016bb3ba97eb65fe3
Chris Ware <c.p.ware@hotmail.com>
2025-12-25 13:45:55 -0700

    logo + section intro fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,12 +2,17 @@ import ContactHero from "@/components/ContactPage/ContactHero";
 import ContactPageContainer from "@/components/ContactPage/ContactPageContainer/ContactPageContainer";
 import BlogSection from "@/components/shared/BlogSection/BlogSection";
 import FinalCTAMain from "@/components/shared/FinalCTAMain/FinalCTAMain";
+import ScrollVelocity from "@/components/shared/ScrollVelocity/ScrollVelocity";
 
 export default function ContactPage() {
   return (
     <div>
       <ContactHero />
       <ContactPageContainer />
+      <ScrollVelocity
+        texts={["Fonts & Footers •", "Fonts & Footers •"]}
+        className='Fonts & Footers •'
+      />
       <BlogSection />
       <FinalCTAMain />
     </div>
--- src/components/shared/Logo/Logo.tsx
+++ src/components/shared/Logo/Logo.tsx
@@ -1,3 +1,4 @@
+import SectionIntroii from "../SectionIntroii/SectionIntroii";
 import styles from "./Logo.module.css";
 import Link from "next/link";
 
@@ -5,7 +6,8 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <Link href='/' className={styles.text}>
-      Fonts & Footers
+      {/* Fonts & Footers */}
+      <SectionIntroii title='Fonts & Footers' color='white' />
     </Link>
   );
 };
